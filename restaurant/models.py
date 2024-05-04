155c0a8d80ff9d75d74548a8e668329bb5aa45fb
dolinsk-15 <156270537+dolinsk-15@users.noreply.github.com>
2024-05-03 20:33:02 -0700

    from another branch

--- restaurant/models.py
+++ restaurant/models.py
@@ -9,3 +9,14 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
+    
+class just(models.Model):
+    title = models.CharField(max_length=100)  # Заголовок статьи
+    author = models.CharField(max_length=100)  # Автор статьи
+    content = models.TextField()  # Содержание статьи
+    published_date = models.DateField(auto_now_add=True)  # Дата публикации статьи
+
+    def __str__(self):
+        return self.title
