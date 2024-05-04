835fbdc191478ff274cb9be61c3600d372faf6af
dolinsk-15 <156270537+dolinsk-15@users.noreply.github.com>
2024-05-03 19:46:21 -0700

    edited models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)  # Заголовок статьи
+    author = models.CharField(max_length=100)  # Автор статьи
+    content = models.TextField()  # Содержание статьи
+    published_date = models.DateField(auto_now_add=True)  # Дата публикации статьи
+
+    def __str__(self):
+        return self.title
